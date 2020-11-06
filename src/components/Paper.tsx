5ac73db5388e002282cfe6e498f8e1eb77c93b15
cvr <seeve.c@gmail.com>
2020-11-05 20:20:20 -0500

    add generic paper component

--- src/components/Paper.tsx
+++ src/components/Paper.tsx
@@ -1,10 +1,9 @@
-import { Box, BoxProps } from "@chakra-ui/core";
-
-export let Paper = (props: BoxProps) => (
-  <Box
-    padding="2"
-    rounded="2"
-    boxShadow="0px 2px 12px rgba(0, 0, 0, 0.05);"
-    {...props}
-  />
+export let Paper: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
+  children,
+  className,
+  ...props
+}) => (
+  <div {...props} className={`rounded p-3 shadow-sm bg-white ${className}`}>
+    {children}
+  </div>
 );
