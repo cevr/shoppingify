a3e99fff731bc599947c096f347eac2e882204f7
cvr <seeve.c@gmail.com>
2020-11-05 20:20:39 -0500

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ["Quicksand", "sans-serif"],
+      body: ["Quicksand", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        brand: {
+          primary: "#F9A109",
+          secondary: "#FFF0DE",
+          accent: "#80485B",
+          info: "#56CCF2",
+        },
+      },
+    },
+  },
+  variants: {
+    borderColor: ["responsive", "hover", "focus", "focus-within"],
+    textColor: ["responsive", "hover", "focus", "focus-within"],
+  },
+  plugins: [],
+};
